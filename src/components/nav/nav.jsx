bb60f997fc1fb9b7836a1d0457ffd96df828bd84
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-26 23:17:37 -0500

    add a logo to nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom"
+import Logo from '../../assets/UMSLogo.png'
 
 const Nav = () => {
   return(
-    <nav>
-      <NavLink to="/">Home(Logo)</NavLink>
+    <nav className="navigation-bar">
+      <NavLink id='logo' to="/">
+        <img src={Logo} alt="logo" />
+        </NavLink>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/About">About</NavLink>
       <NavLink to="/Services">Services</NavLink>
