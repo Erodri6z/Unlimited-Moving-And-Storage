b9279e211b282b31a73d05804a3f486de092174a
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-27 21:06:55 -0500

    add more content and css to landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,8 +3,13 @@ import landing from '../../assets/landing.webp'
 const Landing = () => {
   return(
     <div className="landing" style={{ backgroundImage: `url(${landing})` }}>
-      <h1 id="landing-title">Welcome To United <br/> Moving & Storage</h1>
-      <h3 id="slogan">Your Satisfaction Is Our #1 Priority</h3>
+      <div id="landing-text">
+        <h1 id="landing-title">Welcome To United <br/> Moving & Storage</h1>
+        <h3 id="slogan">Your Satisfaction Is Our #1 Priority</h3>
+        <a href="/Contact" id="landing-contact">
+          <button id="landing-btn">Book Now</button>
+        </a>
+      </div>
     </div>
   )
 }
