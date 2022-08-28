6891dc92f166c3b3fe424082144ca328ea1d7d80
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-28 13:08:46 -0500

    add styling to about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,21 @@
+import '../../styles/about.css'
+import fragile from "../../assets/fragile.jpg" 
 const About = () => {
   return(
+    <>
     <div className="About">
-      <h1>About</h1>
-      <p>With over 20 years of experience, Unlimited Moving & Storage has been a go-to company for people from all walks of life. Unlimited Moving has helped countless individuals, families, and offices, relocate. Because we have pricing to fit all budgets, impeccable service, and care about our client's peace of mind, we’ve become a staple to all our customers and continue to strive for the utmost quality of service.</p>
-      <br />
-      <p>We provide every service you could possibly need, have the capable tools to move any household item, and we take great care in packaging and safeguarding your most precious belongings.</p>
-      <br />
-      <p>We hope you’ll call for a quote and allow us to assist you in your move!</p>
+      <div id="about-text">
+        <h1>About</h1>
+        <p>With over 20 years of experience, Unlimited Moving & Storage has been a go-to company for people from all walks of life. Unlimited Moving has helped countless individuals, families, and offices, relocate. Because we have pricing to fit all budgets, impeccable service, and care about our client's peace of mind, we’ve become a staple to all our customers and continue to strive for the utmost quality of service.</p>
+        <p>We provide every service you could possibly need, have the capable tools to move any household item, and we take great care in packaging and safeguarding your most precious belongings.</p>
+        <p>We hope you’ll call for a quote and allow us to assist you in your move!</p>
+      </div>
+    <div id="fragile">
+      <img src={fragile} id="fragile-image" alt="fragile" />
     </div>
+    </div>
+    
+    </>
   )
 }
 
