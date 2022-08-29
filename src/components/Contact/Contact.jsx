d95bd0bd2a92a6428a2580b42270905354c6e40d
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-29 14:28:22 -0500

    add a responsive nav bar

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -18,9 +18,9 @@ const Contact = () => {
       title="Location"
       width="100%" 
       height="400" 
-      allowfullscreen=" true" 
+      allowFullScreen=" true" 
       loading="lazy" 
-      referrerpolicy="no-referrer-when-downgrade">
+      referrerPolicy="no-referrer-when-downgrade">
       </iframe>
     </div>
     </>
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -2,18 +2,30 @@ import { NavLink } from "react-router-dom"
 import Logo from '../../assets/UMSLogo.png'
 
 const Nav = () => {
+  const navBar = document.getElementById('nav')
+  
+  const toggleNav = (e) => {
+    navBar.classList.toggle('active')
+
+  }
   return(
     <nav className="navigation-bar">
       <NavLink to="/">
         <img id="logo" src={Logo} alt="logo" />
       </NavLink>
-      <selection id="nav">
-        <NavLink className="nav-item" to="/">Home</NavLink>
-        <NavLink className="nav-item" to="/About">About</NavLink>
-        <NavLink className="nav-item" to="/Services">Services</NavLink>
-        <NavLink className="nav-item" to="/Contact">Contact</NavLink>
-      </selection>
-      
+      <button  className="toggle-button" onClick={toggleNav} >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <div id="nav">
+        <ul>
+          <li><NavLink className="nav-item" to="/">Home</NavLink></li>
+          <li><NavLink className="nav-item" to="/About">About</NavLink></li>
+          <li><NavLink className="nav-item" to="/Services">Services</NavLink></li>
+          <li><NavLink className="nav-item" to="/Contact">Contact</NavLink></li>
+        </ul>
+      </div>
     </nav>
   )
 }
