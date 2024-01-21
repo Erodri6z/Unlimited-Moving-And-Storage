9d003d9c75d3c09511e5b30f982b2632f218a5ce
Erodri6z <erikrodriguez2417@gmail.com>
2024-01-21 12:50:05 -0600

    fix typo in landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,7 +4,7 @@ const Landing = () => {
   return(
     <div className="landing" style={{ backgroundImage: `url(${landing})` }}>
       <div id="landing-text">
-        <h1 id="landing-title">Welcome To United <br/> Moving & Storage</h1>
+        <h1 id="landing-title">Welcome To Unlimited <br/> Moving & Storage</h1>
         <h3 id="slogan">Your Satisfaction Is Our #1 Priority</h3>
         <a href="/Contact" id="landing-contact">
           <button id="landing-btn">Book Now</button>
